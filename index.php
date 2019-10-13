278b6e50b37b3296999b180e955ec2abad8d71be
ameeykumar <32795259+ameeykumar@users.noreply.github.com>
2019-10-13 15:54:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,64 @@
-Coming soon
+<!DOCTYPE html>
+<html class="wah">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>coming soon</title>
+
+		<meta name="generator" content="comingsoonpage.com 1.0.0" />
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<!-- Font Awesome CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<!-- Bootstrap and default Style -->
+	 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+	<!-- Google Fonts -->
+					<link class="gf-text" href='https://fonts.googleapis.com/css?family=Open+Sans:400&subset=' rel='stylesheet' type='text/css'>
+	<!-- RTL -->
+	
+	<!-- JS -->
+	<!-- Modernizr -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+	<!-- Retina JS -->
+	<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/retina.js/1.3.0/retina.min.js"></script>-->
+	<!-- Tubular -->
+	
+
+<!-- Header Scripts -->
+	
+<!-- Google Analytics -->
+</head>
+<body>
+	<div id="wah-page">
+		<div id="wah-content">
+						    			    			    				
+						    			    <h1 id="wah-headline">Coming Soon</h1>			    				
+						    			    <div id="wah-description">Get ready! Something really cool is coming!</div>			    				
+			
+		</div><!-- end of #seed-wah-content -->
+			</div>
+
+	<script>jQuery(document).ready(function($){$("#wah-description,#wah-thankyoumsg").fitVids();});</script>
+	<!-- Animate -->
+		
+	<script>
+	function resize(){
+			$('head').append("<style id='form-style' type='text/css'></style>");
+			$('#form-style').html('.wah .input-group-btn, .wah .input-group{display:block;width:100%;}.wah #wah-subscribe-btn{margin-left:0;width:100%;display:block;}.wah .input-group .form-control:first-child, .wah .input-group-addon:first-child, .wah .input-group-btn:first-child>.btn, .wah .input-group-btn:first-child>.dropdown-toggle, .wah .input-group-btn:last-child>.btn:not(:last-child):not(.dropdown-toggle) {border-bottom-right-radius: 4px;border-top-right-radius: 4px;}.wah .input-group .form-control:last-child, .wah .input-group-addon:last-child, .wah .input-group-btn:last-child>.btn, .wah .input-group-btn:last-child>.dropdown-toggle, .wah .input-group-btn:first-child>.btn:not(:first-child) {border-bottom-left-radius: 4px;border-top-left-radius: 4px;}');
+	}
+	
+	$('#wah-content').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+		var width = $('#wah-field-wrapper').width();
+		//console.log(width);
+		if(width < 480 && width != 0){
+			resize();
+		}
+	}
+	);
+	</script>
+	
+	</body>
+</html>
